b64adc57539d3811f65fa5c41fd25e78ce0cadcc
Kowbi-24 <kobelester22@gmail.com>
2025-03-06 21:58:04 +0800

    Added choose algo method

--- Main.java
+++ Main.java
@@ -1,7 +1,31 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int chosenAlgo = chooseAlgo();
+
+        if (chosenAlgo == 1){
+            // MLQ Goes here
+        }
+        else if (chosenAlgo == 2){
+            // Priority Queue goes here
+        }
+        else{
+            // Invalid input goes here
+        }
+    }
+
+    
+    public static int chooseAlgo(){
+
+        // Ask user to choose algorithm
+        System.out.println("[1] MLQ     [2] Priority Queue");
+        System.out.print("Choose Algorithm: ");
+        int chosenAlgo = scanner.nextInt();
+
+        return chosenAlgo;
     }
 }
